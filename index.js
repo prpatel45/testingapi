f23b5af1d1fe322b19d2a5e041a6c90a272812bb
prpatel45 <ppanseriya601@rku.ac.in>
2021-01-31 20:39:44 +0530

    changes in index.js file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const route = require('./route');
 mongoose.connect("mongodb+srv://user:Prp@12@cluster0.usk10.mongodb.net/student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     ()=>{
         const app = express();
+        app.use(express.urlencoded({ extended: true }));
         app.use("/api",route);
         app.listen(process.env.port || 3000,()=>{
             console.log("server started!!!...");
