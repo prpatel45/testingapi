dccdd0dc2140b1f9f66660d18d38eea4d8bda431
prpatel45 <ppanseriya601@rku.ac.in>
2021-02-02 18:09:24 +0530

    changes done

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./route');
+var bodyparser = require('body-parser')
 
 //connect to mongoDB
 mongoose.connect("mongodb+srv://user:Prp@12@cluster0.usk10.mongodb.net/student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     ()=>{
         const app = express();
-        app.use(express.json())
-        app.use(express.urlencoded({ extended: true }));
+        app.get("/",(req,res)=>{
+            res.send("address");
+        })
         app.use("/api",route);
         app.listen(process.env.port || 3000,()=>{
             console.log("server started!!!...");
